50310893e8e7094bfe14a31d4fd63f3412fabc4a
Amardip Yadav <145245944+Amardipy10@users.noreply.github.com>
2026-01-11 14:27:17 +0530

    Update backend URL for production environment

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000"
-    : "https://chatify-4zf5.onrender.com";
+    : "https://chatify-backend-pfdn.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
